8c9b8fcbf992952ce7903fbd0fbda770237e0004
Dorian Gray <d0r1an.gray@yandex.ru>
2023-10-26 22:45:27 +0300

    Задача №1 решена! Программа выводит целые числа между -N и N

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//На входе целое число N. Надо вывести все целые числа в промежутке от -N до N
+
+System.Console.WriteLine("Введите целое число:");
+int N = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine(N);
+int start = N*(-1);
+while(N >= start){
+    System.Console.Write(start + " ");
+    start = start+1;
+}
+if(N < 0){
+    System.Console.WriteLine("Вы ввели отрицательное число. Программа прекратила свою работу!");
+}
+if(N == 0){
+    System.Console.WriteLine("Вы ввели 0! Программа прекратила свою работу!");
+}
+else{
+    System.Console.WriteLine("Вывод чисел закончен!");
+}
