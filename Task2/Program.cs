d451460860866098eb0392c57a4afff9dc5be8c5
Dorian Gray <d0r1an.gray@yandex.ru>
2023-11-02 17:47:36 +0300

    Программа, показывающая сумму первой и последней цифры 3-значного числа

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное целое число и на выходе показывает сумму первой и последней цифры
+using System.ComponentModel.DataAnnotations;
+
+System.Console.WriteLine("Ввведите трёхзначное целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number.ToString().Length <3 || number.ToString().Length > 3){
+    System.Console.WriteLine("Вы ввели не трёхзначное число!!!");
+}
+else{
+    System.Console.WriteLine("Результат сложения первой и последней цифры числа: " + (number/100+number%10));
+}
